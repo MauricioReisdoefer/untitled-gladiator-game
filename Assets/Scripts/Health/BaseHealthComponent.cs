84dbc1fa91b5a0aaae999d1c5389644056cc7228
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-09 02:18:31 -0300

    feat: creating base health component

---
+++ Assets/Scripts/Health/BaseHealthComponent.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHealthComponent : MonoBehaviour, IHealthComponent
+{
+    protected float currentHealth;
+    protected float maxHealth;
+
+    protected virtual void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public virtual void Heal(float health)
+    {
+        currentHealth += health;
+        currentHealth = Mathf.Min(currentHealth, maxHealth);
+    }
+
+    public virtual void SufferDamage(float health)
+    {
+        currentHealth -= health;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    public virtual float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public virtual float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+}

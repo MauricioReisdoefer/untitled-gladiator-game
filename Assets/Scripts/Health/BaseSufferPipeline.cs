f421230a96e97c654bde6ef32a63f7b7fee22d9a
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-10 02:12:58 -0300

    fix: adding suffermodifier in suffer pipeline

--- Assets/Scripts/Health/BaseSufferPipeline.cs
+++ Assets/Scripts/Health/BaseSufferPipeline.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class BaseSufferPipeline : MonoBehaviour, ISufferPipeline
 {
     private IHealthComponent healthComponent;
+    private ISufferModifier[] modifiers;
     private void Awake()
     {
+        modifiers = GetComponents<ISufferModifier>();
         healthComponent = GetComponent<IHealthComponent>();
     }
     public void SufferDamage(AttackData attackData)
     {
+        foreach(ISufferModifier modifier in modifiers)
+        {
+            attackData.damage *= modifier.GetSufferModifier();
+        }
         healthComponent.SufferDamage(attackData.damage);
     }
 }
